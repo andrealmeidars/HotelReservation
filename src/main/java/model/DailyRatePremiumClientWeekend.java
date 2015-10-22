769b8714761cb5501dbc890af15ad03166e5fed0
Andre Almeida <andre.almeida@msn.com>
2015-10-22 19:37:42 -0200

    Create DailyRatePremiumClientWeekend class

---
+++ src/main/java/model/DailyRatePremiumClientWeekend.java
@@ -0,0 +1,14 @@
+package model;
+
+
+public class DailyRatePremiumClientWeekend {
+    private double dailyRatePremiumClientWeekend;
+
+    public DailyRatePremiumClientWeekend(double dailyRatePremiumClientWeekend) {
+        this.dailyRatePremiumClientWeekend = dailyRatePremiumClientWeekend;
+    }
+
+    public double getDailyRatePremiumClientWeekend() {
+        return dailyRatePremiumClientWeekend;
+    }
+}
