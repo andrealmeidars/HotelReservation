3b7ac1d5b410714f36df228310b18bb86d79b09b
Andre Almeida <andre.almeida@msn.com>
2015-10-22 19:29:34 -0200

    Create class ClassificationHotelValue

---
+++ src/main/java/model/ClassificationHotelValue.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ClassificationHotelValue {
+    private int classificationValue;
+
+    public ClassificationHotelValue(int classificationValue) {
+        this.classificationValue = classificationValue;
+    }
+
+    public int getClassificationValue() {
+        return classificationValue;
+    }
+}
