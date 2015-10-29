dd972b563920921d6505e50b68a598b7985f5828
Andre Almeida <andre.almeida@msn.com>
2015-10-29 12:20:26 -0200

    refactoring calculateHotels class

--- src/main/java/calculate/daily/HostingHotelCalculate.java
+++ src/main/java/calculate/daily/HostingHotelCalculate.java
@@ -2,32 +2,27 @@ package calculate.daily;
 
 
 import hotel.Hotel;
+import hotel.HotelList;
 import model.DailyRatePremiumHotelValue;
 import model.DailyRateRegularHotelValue;
 
-
+import java.util.List;
 
 
 public class HostingHotelCalculate {
+    private List<Hotel> hotels;
+
+
 
     public String calculateHotels(String clientType, String day1, String day2, String day3){
+
 
         double hostingValue = 0;
         String hotelName = "";
         double classificationHotel = 0;
+        hotels = HotelList.getHotelList();
 
 
-        Hotel Lakewood = new Hotel("Lakewood", 3,
-                new DailyRateRegularHotelValue(110, 90), new DailyRatePremiumHotelValue(80,80));
-
-        Hotel Bridgewood = new Hotel("Bridgewood", 4,
-                new DailyRateRegularHotelValue(160, 60), new DailyRatePremiumHotelValue(110,50));
-
-        Hotel Ridgewood = new Hotel("Ridgewood", 5,
-                new DailyRateRegularHotelValue(220, 150), new DailyRatePremiumHotelValue(100,40));
-
-
-        Hotel[] hotels = {Lakewood, Bridgewood, Ridgewood};
 
         for (Hotel hotel : hotels){
 
---
+++ src/main/java/hotel/HotelList.java
@@ -0,0 +1,33 @@
+package hotel;
+
+
+import model.DailyRatePremiumHotelValue;
+import model.DailyRateRegularHotelValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelList {
+    public static List<Hotel> getHotelList(){
+        List<Hotel> hotels = new ArrayList<>();
+
+        Hotel Lakewood = new Hotel("Lakewood", 3,
+                new DailyRateRegularHotelValue(110, 90), new DailyRatePremiumHotelValue(80,80));
+
+        Hotel Bridgewood = new Hotel("Bridgewood", 4,
+                new DailyRateRegularHotelValue(160, 60), new DailyRatePremiumHotelValue(110,50));
+
+        Hotel Ridgewood = new Hotel("Ridgewood", 5,
+                new DailyRateRegularHotelValue(220, 150), new DailyRatePremiumHotelValue(100,40));
+
+
+        hotels.add(Lakewood);
+        hotels.add(Bridgewood);
+        hotels.add(Ridgewood);
+
+        return hotels;
+    }
+
+
+
+}
