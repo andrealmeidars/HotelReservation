4a2f5b253f9808cbe50c8772584bdeda6a276221
Andre Almeida <andre.almeida@msn.com>
2015-11-05 08:07:52 -0200

    Change ClientType
    Rename insertTypeClient method
    Create returnCustomer to return true or false

--- src/main/java/client/type/ClientType.java
+++ src/main/java/client/type/ClientType.java
@@ -1,37 +1,30 @@
 package client.type;
 
 
-import java.util.Scanner;
-
-
 
 public class ClientType {
+    boolean isClientRegular = false;
 
 
-public String returnTypeClient() throws NullPointerException{
 
-        System.out.println(" Choose the Type of Customer:");
-        System.out.println(" Options:  1 = Regular  - 2 = Premium ");
+    public void insertTypeClient(String customer) {
 
-        Scanner clientType = new Scanner(System.in);
-        String optionClient = clientType.nextLine();
+        if (customer.equals("Regular")) {
+            isClientRegular = true;
 
-        if (optionClient.equals("1")){
-              return "Regular";
-         } else if(optionClient.equals("2")) {
-              return "Premium";
-        } else
-
-            throw new NullPointerException();
-
-
-          //return "";
-
+        } else {
+            isClientRegular = false;
+         }
     }
 
+    public boolean returnCustomer(){
+        return isClientRegular;
 
-
-
-
-
+    }
 }
+
+
+
+
+
+
