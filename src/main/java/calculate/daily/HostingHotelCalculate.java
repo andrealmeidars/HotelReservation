518f00e070ffd6e0e2e02f406867e53bb54dde16
Andre Almeida <andre.almeida@msn.com>
2015-11-14 23:31:33 -0200

    Refactoring findCheapestHotelToClient method

--- src/main/java/calculate/daily/HostingHotelCalculate.java
+++ src/main/java/calculate/daily/HostingHotelCalculate.java
@@ -1,44 +1,40 @@
 package calculate.daily;
 
 
+import client.type.ClientType;
 import hotel.Hotel;
 import hotel.HotelList;
+import hotel.HotelResource;
+
 import java.util.List;
 
 
 public class HostingHotelCalculate {
     private List<Hotel> hotels;
 
-
-
-    public String calculateHotels(boolean clientType, List<String > days){
-
-
+    public String findCheapestHotelToClient(ClientType clientType, List<String> days) {
         double lowerHostingValue = 0;
         String hotelName = "";
-        double classificationHotel = 0;
-        hotels = HotelList.getHotelList();
-
-
-         for (Hotel hotel : hotels){
+        int classificationHotel = 0;
+        hotels = new HotelList().getAll();
+        for (Hotel hotel : hotels) {
 
             double hostingValue = hotel.calculateHostingValue(clientType, days);
 
-            if (  (lowerHostingValue == 0 ) ||  ( lowerHostingValue >= hostingValue) ) {
-                hotelName = hotel.getHotelName();
+            if ((lowerHostingValue == 0) || (lowerHostingValue >= hostingValue)) {
+                hotelName = hotel.getName();
                 lowerHostingValue = hostingValue;
 
-                if (classificationHotel == 0){
-                    classificationHotel = hotel.getClassificationHotelValue();
-                } else if (classificationHotel > hotel.getClassificationHotelValue()) {
-                    hotelName = hotel.getHotelName();
+                if (classificationHotel == 0) {
+                    classificationHotel = hotel.getRates();
+                } else if (classificationHotel > hotel.getRates()) {
+                    hotelName = hotel.getName();
                 }
             }
 
         }
 
-         return hotelName;
-
+        return hotelName;
 
     }
 }
